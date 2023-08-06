ff8a61bf4e517a1a9a8f2699a82790c367ea6239
EC2 Default User <ec2-user@ip-172-31-11-20.ap-northeast-1.compute.internal>
2023-08-06 11:02:34 +0000

    Add 2-12

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,55 @@
+class BookController < ApplicationController
+  def new
+    
+  end
+
+  def create
+    @lists = Book.all
+    # データを受け取り新規登録するためのインスタンス作成
+    @list = Book.new(list_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    if @list.save
+      # フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "Book was successfully created."
+      # 詳細画面へリダイレクト
+      redirect_to book_path(@list.id)
+    else
+      
+      render :index
+      
+    end
+  end
+  
+  def destroy
+    list = Book.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/book'  # 投稿一覧画面へリダイレクト  
+  end
+  
+  def index
+    @lists = Book.all
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = Book.new  
+  end
+
+  def show
+    @list = Book.find(params[:id])  
+  end
+
+  def edit
+    @list = Book.find(params[:id])
+  end
+  
+  def update
+    list = Book.find(params[:id])
+    list.update(list_params)
+    redirect_to book_path(list.id)  
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+end
